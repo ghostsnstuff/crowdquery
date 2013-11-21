a9b80a03b089de756f411efed8f3c1d434f8aa60
jaredhalpert <jaredhalpert90@gmail.com>
2013-11-21 15:06:16 -0800

    Update api.js

--- crawlers/crunchbase/modules/api/api.js
+++ crawlers/crunchbase/modules/api/api.js
@@ -1,21 +1,33 @@
+// modules
 var cheerio = require('cheerio'),
     request = require('request'),
     fs = require('fs');
 
+// api url attachments 
 var api_url1 = 'http://api.crunchbase.com',
     api_key = '?api_key=9tnkpa2fdm7mf4ef2ubsazwy';
 
+// global object declaration
 var Api = function() {};
+
+// localApi instance is used for internal fxn usage
 var localApi = new Api();
+
+// make object global
 exports.Api = Api;
 
+// prints input
 Api.prototype.printData = function(data) {
    console.log(data);
 }
+
+// vars associated with creating jsons
+// crawl count is used to indicate when to write to file
 var bigJSON = [],
     jsonCOUNT = 0,
     crawlCOUNT = 1;		
 
+// prints data in api.json 
 Api.prototype.readJSON = function() {
    fs.readFile('/Users/jaredhalpert/crowdquery/crawlers/crunchbase/modules/api/api.json', function(err, data) {
       if(err) {throw err;}
@@ -24,6 +36,7 @@ Api.prototype.readJSON = function() {
    });
 }
 
+// returns api.json as an array
 Api.prototype.fetchJSON = function() {
     var data = fs.readFileSync('/Users/jaredhalpert/crowdquery/crawlers/crunchbase/modules/query/query.json', 'utf-8');
     data = data.toString();
@@ -31,6 +44,7 @@ Api.prototype.fetchJSON = function() {
     return data;
 }
 
+// returns value assoicated with a given json key
 Api.prototype.fetchKeyValue = function(json, key) {
    for(var data in json) {
       if(data === key) {
@@ -39,6 +53,7 @@ Api.prototype.fetchKeyValue = function(json, key) {
     }
 }
 
+// crawls each profile page, data from query.json
 Api.prototype.goCrawl = function(array) {
     for(var i = 0; i < array.length; i++) {
        var tempHREF = localApi.fetchKeyValue(array[i], 'href');
@@ -46,6 +61,8 @@ Api.prototype.goCrawl = function(array) {
     }
 }
 
+// crawls profile page, fetches api href
+// transforms data to nested json
 Api.prototype.crawl = function(err, res, html) {
     var rawData = cheerio.load(html);
     var links = localApi.fetchDataLinks(rawData);
@@ -58,6 +75,7 @@ Api.prototype.crawl = function(err, res, html) {
     }
 }
 
+// returns an array of all profile page hrefs
 Api.prototype.fetchDataLinks = function(data) {
    var links = [];
    var moreLinks = true;
@@ -74,6 +92,7 @@ Api.prototype.fetchDataLinks = function(data) {
    return links;
 }
 
+// finds api href match, returns result
 Api.prototype.apiMatch = function(hrefs) {
    for(var i = 0; i < hrefs.length; i++) {
       var curr_href = hrefs[i],
@@ -89,6 +108,7 @@ Api.prototype.apiMatch = function(hrefs) {
    }
 }
 
+// transforms data to nested json
 Api.prototype.addJSON = function(data) {
    var tempJSON = {};
    tempJSON.api = data;
@@ -96,6 +116,7 @@ Api.prototype.addJSON = function(data) {
    jsonCOUNT++;
 }
 
+// writes nested json to api.json
 Api.prototype.writeJSON = function(data) {
    var finalJSON = JSON.stringify(data);
    fs.writeFile('/Users/jaredhalpert/crowdquery/crawlers/crunchbase/modules/api/api.json', finalJSON, function(err) {
