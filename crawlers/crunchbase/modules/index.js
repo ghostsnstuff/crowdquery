37f41b7880c814dd2236edbc439ad63a5cc372fa
jaredhalpert <jaredhalpert90@gmail.com>
2013-11-21 14:18:58 -0800

    Update index.js

--- crawlers/crunchbase/modules/index.js
+++ crawlers/crunchbase/modules/index.js
@@ -1,12 +1,18 @@
+// Query module
 var Query = require('./query/query').Query,
     query = new Query();
 
+// initial crawl
 var numRequests = 66;
 query.crawlCB(numRequests);
-		
+
+// Api module
 var Api = require('./api/api').Api,
     api = new Api();
 
+// fetch array of json from initial crawl
 var data = api.fetchJSON();
+
+// crawl api hrefs
 api.goCrawl(data);
 
