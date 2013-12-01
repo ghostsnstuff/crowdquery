c315995168429ecffa493b62d81d5952a8830efc
jaredhalpert <jaredhalpert90@gmail.com>
2013-12-01 14:34:45 -0800

    Update location.js

--- crawlers/kickstarter/project_burglar/location/location.js
+++ crawlers/kickstarter/project_burglar/location/location.js
@@ -41,15 +41,15 @@ function parseLocation(data) {
 function fromTheUS(state) {
 	var isFromTheUS = false;
 	var states = ["AL","AK","AZ","AR","CA",
-  							"CO","CT","DE","FL","GA",
-  							"HI","ID","IL","IN","IA",
-  							"KS","KY","LA","ME","MD",
-			  				"MA","MI","MN","MS","MO",
-			  				"MT","NE","NV","NH","NJ",
-			  				"NM","NY","NC","ND","OH",
-			  				"OK","OR","PA","RI","SC",
-			  				"SD","TN","TX","UT","VT",
-			  				"VA","WA","WV","WI","WY"];
+  		      "CO","CT","DE","FL","GA",
+  		      "HI","ID","IL","IN","IA",
+  		      "KS","KY","LA","ME","MD",
+  		      "MA","MI","MN","MS","MO",
+  		      "MT","NE","NV","NH","NJ",
+  		      "NM","NY","NC","ND","OH",
+  		      "OK","OR","PA","RI","SC",
+  		      "SD","TN","TX","UT","VT",
+  		      "VA","WA","WV","WI","WY"];
 	for(var i in states) {
 		if(states[i] === state.toUpperCase()) {
 			isFromTheUS = true;
